f0a57eacbd6b0f79a731190d9eec01ca98f0f938
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-12-12 18:05:52 -0800

    register function created. bcrypt installed

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { z } from "zod";
-import { createSession, deleteSession } from "../lib/sessions";
+import bcrypt from "bcrypt";
 import { redirect } from "next/navigation";
+import { createSession, deleteSession } from "../lib/sessions";
 
 const testUser = {
   id: "1",
@@ -10,7 +11,7 @@ const testUser = {
   password: "password",
 };
 
-const loginSchemaa = z.object({
+const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }).trim(),
   password: z
     .string()
@@ -18,8 +19,48 @@ const loginSchemaa = z.object({
     .trim(),
 });
 
+const registerSchema = z.object({
+  firstName: z.string().trim(),
+  lastName: z.string().trim(),
+  email: z.string().email({ message: "Invalid email address" }).trim(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .trim(),
+});
+
+export async function register(prevState: any, formData: FormData) {
+  try {
+    const result = registerSchema.safeParse(Object.fromEntries(formData));
+
+    if (!result.success) {
+      return { errors: result.error.flatten().fieldErrors };
+    }
+
+    const { email, firstName, lastName, password } = result.data;
+
+    if (email === testUser.email) {
+      return { errors: { email: ["Email already in use"] } };
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = {
+      email,
+      firstName,
+      lastName,
+      password: hashedPassword,
+    };
+
+    // TODO: Save user to database and get the return with the user id.
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function login(prevState: any, formData: FormData) {
-  const result = loginSchemaa.safeParse(Object.fromEntries(formData));
+  const result = loginSchema.safeParse(Object.fromEntries(formData));
 
   if (!result.success) {
     return { errors: result.error.flatten().fieldErrors };
