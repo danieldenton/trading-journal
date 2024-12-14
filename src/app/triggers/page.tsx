ac6ecba478d8a32cd4ac983073c50a9b9898f9e3
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-12-13 17:52:53 -0800

    updated trigger notes

--- src/app/triggers/page.tsx
+++ src/app/triggers/page.tsx
@@ -14,6 +14,6 @@ export default function TriggersPage() {
     // 1. Make an input to create a new trigger that just has an input for the name
     // 2. Make a list of triggers
     // 3. Next to each trigger there should be 3 comlumns: 1. successCount 2. failureCount 3. Win Rate successCount/failureCount.
-    //  The list should be sorted by win rate.
+    // 4. The list should be sorted by win rate.
   );
 }
