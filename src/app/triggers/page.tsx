823f4dde9106a0ae2e8117b787cf8d6eee937cd1
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-12-13 17:51:17 -0800

    updated trigger notes

--- src/app/triggers/page.tsx
+++ src/app/triggers/page.tsx
@@ -3,7 +3,7 @@ const placeholderTriggers = [
   { name: "Inverse Fair Value Gap", successCount: 24, failureCount: 13 },
   { name: "SMT", successCount: 30, failureCount: 2 },
   { name: "Breaker", successCount: 15, failureCount: 5 },
-  { name: "AMD", successCount: 6, failureCount: 1 },
+  { name: "Accumulation, Manipulation, Distribution", successCount: 6, failureCount: 1 },
 ];
 
 export default function TriggersPage() {
@@ -11,9 +11,9 @@ export default function TriggersPage() {
     <h1>Triggers Page</h1>
 
     // TODO: Basically a todo list.
-    // 1. Make an input to create a new trigger
+    // 1. Make an input to create a new trigger that just has an input for the name
     // 2. Make a list of triggers
-    // 3. Next to each trigger there should be 3 comlumns: 1. successCount 2. failureCount 3. Win Rate
+    // 3. Next to each trigger there should be 3 comlumns: 1. successCount 2. failureCount 3. Win Rate successCount/failureCount.
     //  The list should be sorted by win rate.
   );
 }
