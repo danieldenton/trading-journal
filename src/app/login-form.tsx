049408e20cf6c7db25da870de950e14773e6d663
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-12-18 07:52:59 -0800

    initial login form style

--- src/app/login-form.tsx
+++ src/app/login-form.tsx
@@ -6,17 +6,19 @@ import { login } from "@/app/lib/actions";
 
 export default function LoginForm() {
   const [state, loginAction, isPending] = useActionState(login, undefined);
+
   return (
-    <form action={loginAction}>
-      <div>
-        <input type="email" name="email" placeholder="Email" />
+    <form action={loginAction} className="flex flex-col items-center justify-center bg-gray-100 p-10 rounded-lg border-4 border-red-600">
+      <div className="m-2">
+        <input type="email" name="email" placeholder="Email" className="p-1 rounded-lg border-black border-2" />
       </div>
-      {state?.errors?.email && (<p className="text-red-500">{state.errors.email}</p>)}
-      <div>
-        <input type="password" name="password" placeholder="Password" />
+      {/* {state?.errors?.email && (<p className="text-red-500">{state.errors.email}</p>)} */}
+      
+      <div className="m-2">
+        <input type="password" name="password" placeholder="Password" className="p-1 rounded-lg border-black border-2" />
       </div>
-      {state?.errors?.email && (<p className="text-red-500">{state.errors.email}</p>)}
-      <button disabled={isPending} type="submit">
+      {/* {state?.errors?.email && (<p className="text-red-500">{state.errors.email}</p>)} */}
+      <button disabled={isPending} type="submit" className="m-2 p-1 bg-red-600  font-bold text-white rounded-lg border-black border-2">
         Login
       </button>
     </form>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import LoginForm from "./login-form";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center justify-center bg-gray-100 p-10 rounded-lg border-4 border-red-600">
-      <h1 className="m-10 font-bold text-black text-4xl">Trade Journal</h1>
+      <div>
+      <h1 className="m-10 font-bold text-grey-100 text-4xl">Trade Journal</h1>
       <LoginForm />
       </div>
     </div>
