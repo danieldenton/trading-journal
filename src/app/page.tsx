15c5d5b3db2412d1636f9a712729a6815bc93b6e
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-12-18 07:37:03 -0800

    login page card started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import LoginForm from "./login-form";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center bg-gray-100 p-10 rounded-lg border-4 border-red-600">
+      <h1 className="m-10 font-bold text-black text-4xl">Trade Journal</h1>
       <LoginForm />
+      </div>
     </div>
   );
 }
