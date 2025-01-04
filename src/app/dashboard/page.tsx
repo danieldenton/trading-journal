893ef8180441be787790e91a2671b28ce3ea5a31
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-03 17:40:21 -0800

    logout-button created and imported into dashbaord page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { logout } from '../lib/actions';
+import React from "react";
+import LogoutButton from "./logout-button";
 
 export default function DashboardPage() {
   return (
-    <h1>Dashboard Page</h1>
-    // total trades
-    // total days traded
-    // win rate
-    // total P&L
-
+    <>
+      <h1>Dashboard Page</h1>
+      // total trades 
+      // total days traded 
+      // win rate 
+      // total P&L
+      <LogoutButton />
+    </>
   );
 }
