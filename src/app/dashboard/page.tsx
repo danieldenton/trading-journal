92355358f89718278c6393d6226f5e8cd298aa2a
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-03 17:41:21 -0800

    dahsboard page comments fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,10 +5,12 @@ export default function DashboardPage() {
   return (
     <>
       <h1>Dashboard Page</h1>
+      {/* 
       // total trades 
       // total days traded 
       // win rate 
-      // total P&L
+      // total P&L 
+      // */}
       <LogoutButton />
     </>
   );
