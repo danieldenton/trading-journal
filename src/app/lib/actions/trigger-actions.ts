e4053bb329553fd0da31311da8613069fef9ffbf
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-24 08:12:10 -0800

    trigger actions

---
+++ src/app/lib/actions/trigger-actions.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { sql } from "@vercel/postgres";
+import { triggerSchema } from "../schema/trigger-schema";
+
+export async function createTrigger(prevState: any, formData: FormData) {
+  try {
+    const result = triggerSchema.safeParse(Object.fromEntries(formData));
+
+    if (!result.success) {
+      console.log(result.error.flatten().fieldErrors);
+      return { errors: result.error.flatten().fieldErrors };
+    }
+
+    const { name, successCount, failureCount, userId } = result.data;
+
+    const existingTrigger = await sql`
+        SELECT 1 FROM triggers WHERE name= ${name} AND user_id = ${userId};
+      `;
+
+    if (existingTrigger.rows.length > 0) {
+      console.log("Trigger already exists");
+      return { errors: { name: ["Trigger already exists"] } };
+    }
+
+    const response = await sql`
+            INSERT INTO triggers (name, successCount, failureCount)
+            VALUES (${name}, ${successCount}, ${failureCount}, ${userId})
+             RETURNING id, name, successCount, failureCount;
+          `;
+          
+    const trigger = response.rows[0];
+
+    if (!trigger) {
+      console.log("Failed to create trigger");
+      return { errors: { name: ["Failed to create trigger"] } };
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getTriggers(userId: number) {
+  try {
+    const response = await sql`
+            SELECT * FROM triggers WHERE user_id = ${userId};
+            `;
+
+    console.log(response.rows);
+    const triggers = response.rows[0];
+
+    if (!triggers) {
+      console.log("User has no triggers");
+      return [];
+    }
+
+    return triggers;
+  } catch (error) {
+    console.error(error);
+  }
+}
