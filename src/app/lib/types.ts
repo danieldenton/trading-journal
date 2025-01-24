c00e7e225c272a506dc27f33ef566c833212d844
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-24 08:07:17 -0800

    types.ts

---
+++ src/app/lib/types.ts
@@ -0,0 +1,13 @@
+export type User = {
+    id: number;
+    email: string;
+    first_name: string;
+  };
+  
+  export type Trigger = {
+    name: string;
+    successCount: number;
+    failureCount: number;
+    winRate: number | undefined
+  };
+  
