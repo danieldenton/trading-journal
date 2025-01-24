b6ce0c54519aee7752fad35c7cba7eeed6b41b5d
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-24 08:31:51 -0800

    logout button and dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import LogoutButton from "./logout-button";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      {/* 
+      // total trades 
+      // total days traded 
+      // win rate 
+      // total P&L 
+      // */}
+      <LogoutButton />
+    </>
+  );
+}
