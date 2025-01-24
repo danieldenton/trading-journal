9be12dcf7876d1e12e393d0e612e6b21dfabe510
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-24 08:46:19 -0800

    triggers page

---
+++ src/app/trade/page.tsx
@@ -0,0 +1,8 @@
+// create inputs for triggers and mistakes
+// Create checkboxes forn existing triggers and mistakes
+
+export default function TradePage() {
+    return (
+      <h1>Trade Form Page</h1>
+    );
+  }
---
+++ src/app/triggers/page.tsx
@@ -0,0 +1,12 @@
+import NewTriggerInput from "../components/new-trigger-input";
+import TriggersTable from "../components/triggers-table";
+
+export default function TriggersPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Triggers</h1>
+      <NewTriggerInput />
+      <TriggersTable />
+    </div>
+  );
+}
