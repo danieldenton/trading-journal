060005a7fff4f4918bc8244c96eb53e7403718ce
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-24 08:40:43 -0800

    register/page and form

---
+++ src/app/register/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { registerUser } from '../lib/actions/auth-actions';
+import RegisterForm from './register-form';
+
+export default function RegisterPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div>
+          <h1 className="m-10 font-bold text-grey-100 text-4xl">Trade Journal</h1>
+          <RegisterForm registerUser={registerUser}/>
+          </div>
+        </div>
+      );
+}
