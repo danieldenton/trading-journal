59e87031c711de2194154e055d38de26549e7095
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-24 08:09:51 -0800

    trigger-schema

---
+++ src/app/lib/schema/trigger-schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const triggerSchema = z.object({
+    name: z.string().trim(),
+    successCount: z.number(),
+    failureCount: z.number(),
+    userId: z.number()
+  });
