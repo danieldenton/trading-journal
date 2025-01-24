80499a13e6f172df513ce6edd62e133255642012
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-24 08:03:29 -0800

    placeholders

---
+++ src/app/lib/placeholders.ts
@@ -0,0 +1,18 @@
+export const mistakePlaceholder = [
+    { name: "no CISD", count: 2 },
+    { name: "entered too early  ", count: 7 },
+    { name: "stop loss too tight", count: 3 },
+  ];
+  
+  export const placeholderTriggers = [
+    { name: "Fair Value Gap", successCount: 25, failureCount: 11 },
+    { name: "Inverse Fair Value Gap", successCount: 24, failureCount: 13 },
+    { name: "SMT", successCount: 30, failureCount: 2 },
+    { name: "Breaker", successCount: 15, failureCount: 5 },
+    {
+      name: "Accumulation Manipulation Distribution",
+      successCount: 6,
+      failureCount: 1,
+    },
+  ];
+  
---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { cookies } from "next/headers";
+// import { decrypt } from "./app/lib/sessions";
+
+// const protectedRoutes = [
+//   "/dashboard",
+//   "/day",
+//   "/trade",
+//   "/stats",
+//   "/triggers",
+//   "/mistakes",
+//   "/setups",
+// ];
+// const publicRoutes = ["/", "/register"];
+
+export default async function middleware(request: NextRequest) {
+  try {
+    console.log("hey middleware");
+    // const path = request.nextUrl.pathname;
+    // const isProtectedRoute = protectedRoutes.includes(path);
+    // const isPublicRoute = publicRoutes.includes(path);
+    // const cookieStore = await cookies();
+    // const cookie = cookieStore.get("session")?.value;
+    // console.log(cookie)
+    // const session = await decrypt(cookie);
+
+    // if (isProtectedRoute && !session?.userId) {
+    //   return NextResponse.redirect(new URL("/", request.nextUrl));
+    // }
+
+    // if (isPublicRoute && session?.userId) {
+    //   return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+    // }
+
+    // return NextResponse.next();
+  } catch (error) {
+    console.error(error);
+  }
+}
