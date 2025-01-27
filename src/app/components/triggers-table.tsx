975a41e495803a382c7ac51dba9028b077fff890
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-27 13:16:34 -0800

    updated table style

--- src/app/components/triggers-table.tsx
+++ src/app/components/triggers-table.tsx
@@ -9,23 +9,23 @@ export default function TriggersTable() {
   const triggerTable = triggers.map((trigger, index) => {
     return (
       <tr key={index}>
-        <td className="border border-gray-300 px-4 py-2 text-center">
+        <td className="border border-gray-300 py-2 text-center">
           {trigger.name}
         </td>
-        <td className="border border-gray-300 px-4 py-2 text-center">
+        <td className="border border-gray-300 py-2 text-center">
           <div className="flex items-center justify-center gap-2">
             {trigger.successCount}
           </div>
         </td>
-        <td className="border border-gray-300 px-4 py-2 text-center">
+        <td className="border border-gray-300 py-2 text-center">
           <div className="flex items-center justify-center gap-2">
             {trigger.failureCount}
           </div>
         </td>
-        <td className="border border-gray-300 px-4 py-2 text-center">
+        <td className="border border-gray-300 py-2 text-center">
           {trigger.winRate}%
         </td>
-        <td className="border border-gray-300 px-4 py-2 flex items-center justify-around">
+        <td className="border border-gray-300 py-2 flex items-center justify-center gap-2">
           <button
             className="bg-gray-300 rounded text-black px-4"
             onClick={() => deleteTriggerFromUser(trigger.name)}
@@ -47,19 +47,17 @@ export default function TriggersTable() {
     <table className="table-auto w-full border-collapse border border-gray-300">
       <thead>
         <tr className="bg-white text-black">
-          <th className="border border-gray-300 px-4 py-2 text-center">
+          <th className="border border-gray-300 py-2 text-center">
             Trigger Name
           </th>
-          <th className="border border-gray-300 px-4 py-2 text-center">
+          <th className="border border-gray-300 py-2 text-center">
             Success Count
           </th>
-          <th className="border border-gray-300 px-4 py-2 text-center">
+          <th className="border border-gray-300 py-2 text-center">
             Failure Count
           </th>
-          <th className="border border-gray-300 px-4 py-2 text-center">
-            Win Rate
-          </th>
-          <th className="border border-gray-300 px-4 py-2 text-center"></th>
+          <th className="border border-gray-300 py-2 text-center">Win Rate</th>
+          <th className="border border-gray-300 py-2 w-15% text-center">Update</th>
         </tr>
       </thead>
       <tbody>{triggerTable}</tbody>
