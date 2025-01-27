e069a8a35699e541221f297296088b82fd0472b1
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-27 05:54:57 -0800

    navabr addded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TriggerContextProvider from "./context/trigger";
 import UserContextProvider from "./context/user";
+import Navbar from "./navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <UserContextProvider>
-          <TriggerContextProvider>{children}</TriggerContextProvider>
+          <TriggerContextProvider>
+            <Navbar />
+            {children}
+          </TriggerContextProvider>
         </UserContextProvider>
       </body>
     </html>
---
+++ src/app/navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import LogoutButton from "./dashboard/logout-button";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white py-4 px-8 flex justify-between items-center">
+      <Link href="/dashboard">Home</Link>
+      <Link href="/trades">Trades</Link>
+      <Link href="/setups">Setups</Link>
+      <Link href="/triggers">Triggers</Link>
+      <Link href="/mistakes">Mistakes</Link>
+      <LogoutButton />
+    </nav>
+  );
+}
