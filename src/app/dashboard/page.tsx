548f60799d31eb6c6badae4bb0c83143574465b1
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-31 11:34:12 -0800

    dashoboard styling begin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,14 +7,18 @@ export default function DashboardPage() {
     <div className="p-6 w-full h-screen">
       <h1 className="text-2xl font-bold mb-4 text-center">Dashboard</h1>
       <UserName />
-
-      <Link href={"/today"} className="bg-red-700 p-4 m-4 font-bold hover:text-yellow-200">Begin a new trading day</Link>
-      {/* 
-      // total trades 
-      // total days traded 
-      // win rate 
-      // total P&L 
-      // */}
+      <div className="flex justify-center">
+        <Link
+          href={"/today"}
+          className="bg-red-700 p-4 m-4 font-bold hover:text-yellow-200 rounded"
+        >
+          Begin a new trading day
+        </Link>
+      </div>
+      <p>total trades logged</p>
+      <p>total days traded</p>
+      <p>win rate</p>
+      <p>total P&L</p>
     </div>
   );
 }
