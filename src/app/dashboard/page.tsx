2746f678dc621748d9140000a242d2834cb6e014
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-31 11:30:34 -0800

    dashoboard styling begin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+import Link from "next/link";
 import UserName from "./user-name";
 
 export default function DashboardPage() {
   return (
-    <>
-      <h1>Dashboard Page</h1>
+    <div className="p-6 w-full h-screen">
+      <h1 className="text-2xl font-bold mb-4 text-center">Dashboard</h1>
       <UserName />
+
+      <Link href={"/today"} className="bg-red-700 p-4 m-4 font-bold hover:text-yellow-200">Begin a new trading day</Link>
       {/* 
       // total trades 
       // total days traded 
       // win rate 
       // total P&L 
       // */}
-    </>
+    </div>
   );
 }
