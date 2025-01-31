4bd65d0d4edf7208c52cd9539f31e52c3fe28f03
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-01-31 11:11:11 -0800

    trade context cleared out

--- src/app/context/trade.tsx
+++ src/app/context/trade.tsx
@@ -24,124 +24,21 @@ export default function TradeContextProvider({
 }: {
   children: ReactNode;
 }) {
-  const [triggers, setTriggers] = useState<TriggerWithWinRate[]>([]);
-  const [newTriggerName, setNewTriggerName] = useState("");
+ 
   const { user } = useUserContext();
 
-  function calculateWinRate(
-    successCount: number,
-    failureCount: number
-  ): number {
-    const total = successCount + failureCount;
-    return total > 0 ? Math.round((successCount / total) * 100) : 0;
-  }
-
-  function addWinRateToTriggers(
-    triggersToUpdated: Trigger[] | undefined
-  ): TriggerWithWinRate[] {
-    const triggersWithWinRate = triggersToUpdated?.map((trigger) => ({
-      ...trigger,
-      winRate: calculateWinRate(trigger.successCount, trigger.failureCount),
-    }));
-    const sortedTriggers =
-      triggersWithWinRate?.sort((a, b) => b.winRate - a.winRate) || [];
-    return sortedTriggers;
-  }
-
-  const fetchTriggers = async () => {
-    try {
-      const userTriggers = await getTriggers(user?.id);
-      const triggersWithWinRate = addWinRateToTriggers(userTriggers);
-      setTriggers(triggersWithWinRate);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   useEffect(() => {
     if (user?.id) {
-      fetchTriggers();
+     
     }
   }, [user?.id]);
 
-  const addNewTrigger = async (prevState: any, formData: FormData) => {
-    try {
-      if (!user?.id) {
-        console.error("User needs to be logged in to add a trigger");
-        return "User needs to be logged in to add a trigger";
-      }
-
-      const newTrigger = await createTrigger(formData, user.id);
-      if (newTrigger?.errors) {
-        const { name } = newTrigger.errors;
-        return name[0];
-      }
-
-      if (typeof newTrigger?.name === "string") {
-        setTriggers((prev) => [
-          ...prev,
-          {
-            id: newTrigger.id,
-            name: newTrigger.name,
-            successCount: 0,
-            failureCount: 0,
-            winRate: 0,
-          },
-        ]);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const deleteTriggerFromUser = async (triggerId: number) => {
-    try {
-      if (!user?.id) {
-        console.error("User needs to be logged in to delete a trigger");
-        return "User needs to be logged in to delete a trigger";
-      }
-      await deleteTrigger(triggerId, user.id);
-      setTriggers((prev) => prev.filter((trigger) => trigger.id !== triggerId));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const postAndSaveUpdatedTriggerToTriggers = async (
-    updatedTrigger: TriggerWithWinRate
-  ) => {
-    try {
-      const returnedTrigger = await updateTrigger(updatedTrigger);
-      if (typeof returnedTrigger === "object" && "id" in returnedTrigger) {
-        const triggerWithWinRate = {
-          ...returnedTrigger,
-          winRate: calculateWinRate(
-            returnedTrigger.successCount,
-            returnedTrigger.failureCount
-          ),
-        };
-
-        setTriggers((prevTriggers) =>
-          prevTriggers.map((trigger) =>
-            trigger.id === triggerWithWinRate.id ? triggerWithWinRate : trigger
-          )
-        );
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+  
   return (
     <TradeContext.Provider
       value={{
-        triggers,
-        setTriggers,
-        newTriggerName,
-        setNewTriggerName,
-        addNewTrigger,
-        deleteTriggerFromUser,
-        postAndSaveUpdatedTriggerToTriggers,
+        
       }}
     >
       {children}
