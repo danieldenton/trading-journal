466df6f9968540d5326671ae0a947ad2bd85ffd9
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-02-02 11:41:56 -0800

    EditModal created in setups

---
+++ src/app/setups/edit-modal.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Setup, SetupModalProps } from "../lib/types";
+import { useSetupContext } from "../context/setup";
+
+export default function EditModal({
+  setup,
+  setModalType,
+}: SetupModalProps) {
+  const [newSetupName, setNewSetupName] = useState(setup.name);
+  const { patchAndSaveUpdatedSetupToSetups } = useSetupContext();
+
+  const updatedSetup = {
+    id: setup.id,
+    name: newSetupName,
+    triggerIds: setup.triggerIds,
+    successCount: setup.successCount,
+    failureCount: setup.failureCount,
+    winRate: setup.winRate,
+  };
+
+  const handleCompleteEdit = () => {
+    patchAndSaveUpdatedSetupToSetups(updatedSetup);
+    setModalType(undefined);
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white h-[25%] w-[25%] flex items-center justify-center flex-col rounded shadow-lg">
+        <p className="text-black font-bold text-lg px-2 text-center mb-1">
+          Edit setup Name
+        </p>
+        <input
+          type="text"
+          name="name"
+          value={newSetupName}
+          onChange={(e) => setNewSetupName(e.target.value)}
+          className="p-2 rounded font-bold text-black placeholder-gray-500 w-[50%] text-center focus:outline-none border border-black mb-1"
+        />
+        <button
+          onClick={() => handleCompleteEdit()}
+          className="bg-red-500 text-white font-bold px-12 py-2 m-1 rounded"
+        >
+          Save
+        </button>
+        <button
+          onClick={() => setModalType(undefined)}
+          className="bg-black text-white font-bold px-10 py-2 m-1 rounded"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+}
