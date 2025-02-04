8c2c1c1df6e8e259229c303f768fff57fd02429b
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-02-04 08:14:18 -0800

    Today added to Navbar. New Trigger form and new setup removed from today

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,6 +6,7 @@ export default function Navbar() {
   return (
     <nav className="bg-white text-black font-bold py-4 px-8 flex justify-between items-center">
       <Link href="/dashboard">Home</Link>
+      <Link href="/today">Today</Link>
       <Link href="/trades">Trades</Link>
       <Link href="/setups">Setups</Link>
       <Link href="/triggers">Triggers</Link>
--- src/app/trade/trade-page-triggers.tsx
+++ src/app/trade/trade-page-triggers.tsx
@@ -5,7 +5,6 @@ export default function TradePageTriggers() {
   return (
     <div className="flex flex-col justify-center items-center w-1/4 border-white border-2">
       <h1 className="font-bold p-2 text-xl">Triggers</h1>
-      <NewTriggerInput />
       <MiniTriggerTable setup={undefined} />
     </div>
   );
