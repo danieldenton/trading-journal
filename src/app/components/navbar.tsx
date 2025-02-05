702474f23fe99f6eb639c8851dcc600bd2d90f9b
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-02-04 17:33:13 -0800

    updated navbar to handle user

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import LogoutButton from "./logout-button";
+import { useUserContext } from "../context/user";
 
 export default function Navbar() {
+  const { user } = useUserContext();
   return (
     <nav className="bg-white text-black font-bold py-4 px-8 flex justify-between items-center">
-      <Link href="/dashboard">Home</Link>
-      <Link href="/today">Today</Link>
-      <Link href="/trades">Trades</Link>
-      <Link href="/setups">Setups</Link>
-      <Link href="/triggers">Triggers</Link>
-      <Link href="/mistakes">Mistakes</Link>
-      <LogoutButton />
+      {user ? (
+        <>
+          <Link href="/dashboard">Home</Link>
+          <Link href="/today">Today</Link>
+          <Link href="/trades">Trades</Link>
+          <Link href="/setups">Setups</Link>
+          <Link href="/triggers">Triggers</Link>
+          <Link href="/mistakes">Mistakes</Link>
+          <LogoutButton />
+        </>
+      ) : (
+        <h2 className="w-full text-center">WELCOME TO TRADING JOURNAL JOURNAL</h2>
+      )}
     </nav>
   );
 }
