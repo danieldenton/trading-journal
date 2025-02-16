81d3303a376ce86f46c110ed57f098c2446c6229
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-02-16 10:19:10 -0800

    MiniTriggerTable now accepts new props triggerState and setTriggerState to handle wether the trigger add is coming from setup or trade

--- src/app/components/mini-trigger-table.tsx
+++ src/app/components/mini-trigger-table.tsx
@@ -2,55 +2,35 @@
 import React from "react";
 
 import { useTriggerContext } from "../context/trigger";
-import { useSetupContext } from "../context/setup";
-import { useTradeContext } from "../context/trade";
-import { Setup } from "../lib/types";
+
+type MiniTriggerTableProps = {
+  triggerState: number[];
+  setTriggerState: React.Dispatch<React.SetStateAction<number[]>>;
+};
 
 export default function MiniTriggerTable({
-  setup,
-}: {
-  setup: Setup | undefined;
-}) {
+  triggerState,
+  setTriggerState,
+}: MiniTriggerTableProps) {
   const { triggers } = useTriggerContext();
-  const { setTriggerIds, triggerIds } = useTradeContext();
-  const { setSelectedTriggerIds, selectedTriggerIds } = useSetupContext();
 
-  const addOrRemoveTriggerFromTrade = (triggerId: number) => {
-    if (!triggerIds.includes(triggerId)) {
-      setTriggerIds((prevState) => [...prevState, triggerId]);
+  const handleAddTrigger = (triggerId: number) => {
+    if (!triggerState.includes(triggerId)) {
+      setTriggerState((prevState) => [...prevState, triggerId]);
     } else {
-      setTriggerIds((prevState) => prevState.filter((id) => id !== triggerId));
-    }
-  };
-
-  const addOrRemoveTriggerFromSetup = (triggerId: number) => {
-    if (!selectedTriggerIds.includes(triggerId)) {
-      setSelectedTriggerIds((prevState) => [...prevState, triggerId]);
-    } else {
-      setSelectedTriggerIds((prevState) =>
+      setTriggerState((prevState) =>
         prevState.filter((id) => id !== triggerId)
       );
     }
   };
 
-  const handleAddTrigger = (triggerId: number) => {
-    if (setup) {
-      addOrRemoveTriggerFromSetup(triggerId);
-    } else {
-      addOrRemoveTriggerFromTrade(triggerId);
-    }
-  };
-
   const miniTriggerTable = triggers.map((trigger, index) => {
-    const checked = setup
-      ? selectedTriggerIds.includes(trigger.id)
-      : triggerIds.includes(trigger.id);
     return (
       <tr key={index}>
         <td className="border border-gray-300 p-2 text-center">
           <input
             type="checkbox"
-            checked={checked}
+            checked={triggerState.includes(trigger.id)}
             onChange={() => handleAddTrigger(trigger.id)}
           />
         </td>
