e940acd61e74c27dea4be29ae8065e44619222ff
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-02-19 15:41:17 -0800

    exit-input.tsx created

---
+++ src/app/trade/exit-inputs.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function ExitInputs() {
+  return (
+    <>
+      <input
+        type="number"
+        name="entryPrice"
+        placeholder="Exit Price"
+        className="m-2 p-1 rounded-lg text-black border-black border-2"
+      />
+      <input
+        type="number"
+        name="exitTime"
+        placeholder="Exit Time"
+        className="m-2 p-1 rounded-lg text-black border-black border-2"
+      />
+    </>
+  );
+}
