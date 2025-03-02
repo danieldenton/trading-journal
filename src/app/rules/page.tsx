1ab0e360722a02c201a807eb57100bf1987b7991
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-03-02 15:26:23 -0800

    rule page.tsx

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -1,10 +1,10 @@
-import NewMistakeInput from "../mistakes/new-mistake-input";
+import NewRuleInput from "./new-rule-input";
 import RulesTable from "./rules-table";
 export default function MistakesPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4 text-center">Mistakes</h1>
-      <NewMistakeInput />
+      <NewRuleInput />
       <RulesTable />
     </div>
   );
