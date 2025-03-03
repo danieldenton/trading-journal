fe5f8050399f95a25317215d64985afdeb7c3c6c
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2025-03-03 09:59:44 -0800

    localStaorage remmoved from logout

--- src/app/context/user.tsx
+++ src/app/context/user.tsx
@@ -42,7 +42,6 @@ export default function UserContextProvider({
 
   const handleLogout = () => {
     setUser(undefined);
-    localStorage.removeItem("user");
     logout();
     router.push("/");
   };
